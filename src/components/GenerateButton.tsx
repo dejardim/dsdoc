8f8231075b7a97badae328740e4237059bf22d19
Zambom <erikzambom@hotmail.com>
2023-04-03 17:03:01 -0300

    feat: adding loading feedback in generate button

--- src/components/GenerateButton.tsx
+++ src/components/GenerateButton.tsx
@@ -1,14 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import generateDoc from "../services/openai";
 import { additionalOptions, options } from "../interfaces/options.interfaces";
 import { useNavigate } from "react-router-dom";
+import { ClipLoader } from "react-spinners";
 
 export const GenerateButton: React.FC<buttonProps> = (props) => {
   const navigate = useNavigate()
+  const [loading, setLoading] = useState(false);
 
   const generate = async () => {
+    setLoading(!loading);
+    
     const response = await generateDoc(props.componentName, props.options, props.addOptions);
-
+    
+    setLoading(!loading);
     console.log(response);
     navigate("/app", { state: response });
     /*
@@ -24,7 +29,8 @@ export const GenerateButton: React.FC<buttonProps> = (props) => {
       </div>
       <div className="mx-2 mt-4 mb-2">
         <button onClick={generate} className="bg-[#1E1E1E] h-12 w-full rounded-sm text-white font-semibold" disabled={props.componentName === ''}>
-          Generate
+          { !loading ? "Generate" : "" }
+          <ClipLoader color="#ffffff" loading={loading} />
         </button>
       </div>
     </>
