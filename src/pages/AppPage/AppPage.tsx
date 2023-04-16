4a17c739241fa31a4f27901026640597e6642895
Zambom <erikzambom@hotmail.com>
2023-04-16 14:17:31 -0300

    feat: changing markdown preview component to have the editor part in it

--- src/pages/AppPage/AppPage.tsx
+++ src/pages/AppPage/AppPage.tsx
@@ -1,16 +1,17 @@
-import React from "react";
-import MarkdownPreview from '@uiw/react-markdown-preview';
+import React, { useState } from "react";
+import MarkdownEditor from "@uiw/react-markdown-editor";
 import { useLocation } from "react-router-dom";
 import { Content } from "../../components/Content";
 
 export const App: React.FC = () => {
   const location = useLocation()
   const { state } = location;
+  const [docs, setDocs] = useState(state?.content);
 
   return (
     <div className="mx-auto" style={{maxWidth: '1280px'}}>
       <Content>
-        <MarkdownPreview source={state?.content} />
+        <MarkdownEditor value={docs} onChange={(value, viewUpdate) => setDocs(value)} visible={true} />
       </Content>
     </div>
   );
